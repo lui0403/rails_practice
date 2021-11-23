eb2754ed7bd795cf1e2d35bbf636b773ba463a83
Your Name <lumanpower2004@gmail.com>
2021-11-23 18:39:19 +0900

    comment function error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(post_params)
-    redirect_to post_path(post.id)
+    redirect_to posts_path
   end
 
   def index
